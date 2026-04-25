fa6ee9e9fb8f6e9f521823ccbb01da9a296939a8
Rin161894 <herve@le-strat.net>
2026-04-25 09:53:04 +0200

    Gestion des impressions

--- test_tri_selection.asm
+++ test_tri_selection.asm
@@ -1,10 +1,22 @@
 .data
-msg1:   .asciz "Avant tri : "
-msg2:   .asciz "\nApres tri : "
+algo:        .asciz "Algorithme de tri par sélection, taille tableau : "
+msg1:        .asciz "Avant tri : \n"
+msg2:        .asciz "\nAprès tri : \n"
 
 .text
 main:
-    # --- Affichage "Avant" ---
+    li   s7, 1 # Affiche (1) ou n'affiche pas (0) l'algorithme et la taille du tableau
+    li   s8, 0 # Affiche (1) ou n'affiche pas (0) les tableaux
+
+    beqz s7, no_header
+    # Affichage des informations concernant le tri.
+    la   a0, algo
+    lw   a1, tab_len
+    jal  ra, print_header
+
+no_header:
+    beqz s8, no_tab1
+    # Affichage du tableau original 
     la   a0, msg1
     li   a7, 4                # print_string
     ecall
@@ -13,12 +25,14 @@ main:
     lw   a1, tab_len          # a1 = taille du tableau
     jal  ra, print_tab
 
+no_tab1:
     # --- Appel selection_sort(tab, tab_len) ---
     la   a0, tab
     lw   a1, tab_len
     jal  ra, selection_sort
 
-    # --- Affichage "Apres" ---
+    beqz s8, no_tab2
+    # Affichage du tableau trié 
     la   a0, msg2
     li   a7, 4
     ecall
@@ -27,12 +41,14 @@ main:
     lw   a1, tab_len
     jal  ra, print_tab
 
+no_tab2:
     # --- Sortie ---
     li   a7, 10               # exit
     ecall
 
 # --- Dépendances ---
 .include "tab.asm"
+.include "header.asm"
 .include "print_tab.asm"
 .include "swap.asm"
 .include "tri_selection.asm"
